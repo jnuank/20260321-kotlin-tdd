d7a91b1d283c87d6c4efb5fbe986699d37c806c3
jnuank <ikuta1919@gmail.com>
2026-03-21 23:10:40 +0900

    いったん通る。だけど、これはテストの種類が増えるたびにfromのeveryが増える。つまりループという不吉な臭いが消え去っていない

--- src/test/kotlin/UserServiceBeforeTest.kt
+++ src/test/kotlin/UserServiceBeforeTest.kt
@@ -1,3 +1,6 @@
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkObject
 import org.example.LoginRecord
 import org.example.User
 import org.example.UserWithLastLogin
@@ -22,7 +25,7 @@ class UserLoginRecordTest {
 
         val actual = UserWithLastLogin.from(user, record)
 
-        val expected =UserWithLastLogin(
+        val expected = UserWithLastLogin(
             userId = 25L,
             lastLoginAt = OffsetDateTime.parse("2024-12-10T10:15:30+09:00")
         )
@@ -31,63 +34,64 @@ class UserLoginRecordTest {
 
 
     @Test
-    fun 各ユーザーの最終ログイン履歴を取得する() {
+    fun 各ユーザーの最終ログイン履歴を取得する() = mockkObject(UserWithLastLogin.Companion) {
         val user1 = User(id = 1L, name = "ユーザー1", email = "user1@example.com")
         val user2 = User(id = 2L, name = "ユーザー2", email = "user2@example.com")
         val users = listOf(user1, user2)
 
-        val loginRecords = listOf(
-            LoginRecord(
-                userId = 1L, loginAt = OffsetDateTime.parse("2026-01-02T10:15:30+09:00"), ipAddress = "192.168.1.1"
-            ),
-            LoginRecord(
-                userId = 2L, loginAt = OffsetDateTime.parse("2026-01-22T10:15:30+09:00"), ipAddress = "192.168.1.2"
-            ),
+        val loginRecord1 = LoginRecord(
+            userId = 1L, loginAt = OffsetDateTime.parse("2026-01-02T10:15:30+09:00"), ipAddress = "192.168.1.1"
         )
+        val loginRecord2 = LoginRecord(
+            userId = 2L, loginAt = OffsetDateTime.parse("2026-01-22T10:15:30+09:00"), ipAddress = "192.168.1.2"
+        )
+        val loginRecords = listOf(loginRecord1, loginRecord2)
 
-        val actual = getUsersWithLastLogin(users, loginRecords = loginRecords)
-        val expected = listOf(
-            UserWithLastLogin(
-                userId = 1L, lastLoginAt = OffsetDateTime.parse("2026-01-02T10:15:30+09:00"),
-            ),
-            UserWithLastLogin(
-                userId = 2L, lastLoginAt = OffsetDateTime.parse("2026-01-22T10:15:30+09:00"),
-            )
-        )
+        val userWithLastLogin1 = mockk<UserWithLastLogin>()
+        val userWithLastLogin2 = mockk<UserWithLastLogin>()
+        every { UserWithLastLogin.from(user1, loginRecord1) } returns userWithLastLogin1
+        every { UserWithLastLogin.from(user2, loginRecord2) } returns userWithLastLogin2
+
+        val actual = getUsersWithLastLogin(users, loginRecords)
+
+        val expected = listOf(userWithLastLogin1, userWithLastLogin2)
         assertEquals(expected, actual)
     }
 
     @Test
-    fun 各ユーザーの最終ログイン履歴を取得する_各ユーザーに複数レコードある場合() {
-        val user1 = User(id = 1L, name = "ユーザー1", email = "user1@example.com")
-        val user2 = User(id = 2L, name = "ユーザー2", email = "user2@example.com")
-        val users = listOf(user1, user2)
-
-        val loginRecords = listOf(
-            LoginRecord(
-                userId = 1L, loginAt = OffsetDateTime.parse("2026-01-02T10:15:30+09:00"), ipAddress = "192.168.1.1",
-            ),
-            LoginRecord(
-                userId = 2L, loginAt = OffsetDateTime.parse("2026-01-22T10:15:30+09:00"), ipAddress = "192.168.1.2"
-            ),
-            LoginRecord(
-                userId = 2L, loginAt = OffsetDateTime.parse("2026-03-26T10:15:30+09:00"), ipAddress = "192.168.1.2"
-            ),
-            LoginRecord(
-                userId = 1L, loginAt = OffsetDateTime.parse("2026-02-07T03:23:45+09:00"), ipAddress = "192.168.1.1"
-            ),
-        )
-
-        val actual = getUsersWithLastLogin(users, loginRecords = loginRecords)
-        val expected = listOf(
-            UserWithLastLogin(
-                userId = 1L, lastLoginAt = OffsetDateTime.parse("2026-02-07T03:23:45+09:00")
-            ),
-            UserWithLastLogin(
-                userId = 2L, lastLoginAt = OffsetDateTime.parse("2026-03-26T10:15:30+09:00")
+    fun 各ユーザーの最終ログイン履歴を取得する_各ユーザーに複数レコードある場合() =
+        mockkObject(UserWithLastLogin.Companion) {
+            val user1 = User(id = 1L, name = "ユーザー1", email = "user1@example.com")
+            val user2 = User(id = 2L, name = "ユーザー2", email = "user2@example.com")
+            val users = listOf(user1, user2)
+            val loginRecord1a = LoginRecord(
+                userId = 1L, loginAt = OffsetDateTime.parse("2026-01-02T10:15:30+09:00"), ipAddress = "192.168.1.1"
             )
-        )
-        assertEquals(expected, actual)
-    }
+            val loginRecord2a = LoginRecord(
+                userId = 2L, loginAt = OffsetDateTime.parse("2026-01-22T10:15:30+09:00"), ipAddress = "192.168.1.2"
+            )
+            val loginRecord2b = LoginRecord(
+                userId = 2L, loginAt = OffsetDateTime.parse("2026-03-26T10:15:30+09:00"), ipAddress = "192.168.1.2"
+            )
+            val loginRecord1b = LoginRecord(
+                userId = 1L, loginAt = OffsetDateTime.parse("2026-02-07T03:23:45+09:00"), ipAddress = "192.168.1.1"
+            )
+            val loginRecords = listOf(
+                loginRecord1a,
+                loginRecord2a,
+                loginRecord2b,
+                loginRecord1b,
+            )
+
+            val userWithLastLogin1 = mockk<UserWithLastLogin>()
+            val userWithLastLogin2 = mockk<UserWithLastLogin>()
+            every { UserWithLastLogin.from(user1, loginRecord1b) } returns userWithLastLogin1
+            every { UserWithLastLogin.from(user2, loginRecord2b) } returns userWithLastLogin2
+
+            val actual = getUsersWithLastLogin(users, loginRecords = loginRecords)
+
+            val expected = listOf(userWithLastLogin1, userWithLastLogin2)
+            assertEquals(expected, actual)
+        }
 
 }
